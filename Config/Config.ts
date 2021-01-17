ba5c4e9078db88e3cc6ef64e2acfbff7bc5573f0
zedxos <69835674+zedxos@users.noreply.github.com>
2021-01-17 19:34:59 +0800

    Initial Commit

---
+++ Config/Config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+  discord_token: 'Nzk0NTgzNTgxNjU2NjEyODg1.X-87oA.aijjfZHeGtqCW8Sd9c8sXJMeglg',
+  cmd_cooldown: 3,
+  developer: '704697854207459419',
+  prefix: 'f.'
+}
---
+++ Config/Emojis.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  flandre_angery: '<:flandre_angery:793065665053327370>',
+  flandre_hi: '<:flandre_hi:793065670266847272>',
+  flandre_laugh: '<:flandre_laugh:793065670622445599>',
+  flandre_satisfied: '<:flandre_satisfied:793065670027247626>',
+  flandre_sigh: '<:flandre_sigh:793065671059308574>',
+  flandre_tired: '<:flandre_tired:793065659898396700>',
+  flandre_ugh: '<:flandre_ugh:793065673991389204>',
+  flandre_uhh: '<:flandre_uhhh:793065670925090816>'
+}
